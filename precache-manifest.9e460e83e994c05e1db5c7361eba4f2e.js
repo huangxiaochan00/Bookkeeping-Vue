9d27117c233972665aa58f9b99b119a38e1a8d0f
SranHuang_1005 <763913797@qq.com>
2022-03-05 17:24:17 +0800

    deploy

---
+++ precache-manifest.9e460e83e994c05e1db5c7361eba4f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336b14ef11bef52468db",
+    "url": "/Bookkeeping-Vue/css/app.c14c2019.css"
+  },
+  {
+    "revision": "bb7c90b91e7bcd0df4825aade9c2fb28",
+    "url": "/Bookkeeping-Vue/index.html"
+  },
+  {
+    "revision": "336b14ef11bef52468db",
+    "url": "/Bookkeeping-Vue/js/app.9b0ce1d3.js"
+  },
+  {
+    "revision": "02059acf6e4adbcb3db8",
+    "url": "/Bookkeeping-Vue/js/chunk-vendors.ccbf6d25.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/Bookkeeping-Vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Bookkeeping-Vue/robots.txt"
+  }
+]);
